c437a6699bd28e53fe7d755d1f1df213be95ebe1
Ivan <ivanmleffalle@gmail.com>
2025-03-31 19:13:23 -0300

    last commit

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const session = require("express-session");
 const pool = require("./database/");
 const accountRoute = require("./routes/accountRoute");
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 /* ************************
  * Middleware
@@ -44,6 +45,8 @@ app.use(function (req, res, next) {
   res.locals.messages = require("express-messages")(req, res);
   next();
 });
+
+app.use(cookieParser())
 
 /* ***********************
  * View Engine and Templates
