6b66f5092cc5790da748eac97cc11dd0c591bc8f
Ivan <ivanmleffalle@gmail.com>
2025-03-31 02:09:22 -0300

    password hash

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,8 @@
 const utilities = require("../utilities");
 const accountCont = {};
 const accountModel = require("../models/account-model");
+const bcrypt = require("bcryptjs")
+
 accountCont.buildLogin = async function (req, res, next) {
   let nav = await utilities.getNav();
   // const form = await utilities.loginForm();
@@ -33,11 +35,26 @@ accountCont.registerAccount = async function (req, res) {
     account_password,
   } = req.body;
 
+
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("./account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   );
 
   if (regResult) {
@@ -57,6 +74,7 @@ accountCont.registerAccount = async function (req, res) {
     res.status(501).render("account/register", {
       title: "Registration",
       nav,
+      errors
     });
   }
 };
